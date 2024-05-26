9dad5c97be253f57f2972ca3ae102f63866dfbed
Kevin Quintero <kevinstequintero@gmail.com>
2024-05-26 15:57:21 -0500

    Datos ya tratamos, sin espacios nulos, NA u otros campos

--- Script.R
+++ Script.R
@@ -1,9 +1,16 @@
 library(lsr)
 library(moments)
 library(readr)
+library(dplyr)
 
+#Leemos el archivo ubicado en el github del proyecto
 data <- read.csv("https://raw.githubusercontent.com/kevinQuinteroB/ProyectoEstadistica/main/Delitos_ocurridos_en_el_Municipio_de_Bucaramanga_20240519.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
+#Buscamos los campos que se encuentran vacios
+latitud_vacia <- data %>% filter(LATITUD == "xx.xxxx")
+longitud_vacia <- data %>% filter(LONGITUD == "-yy.yyyy")
+filas_vacias <- data %>% filter(CURSO_DE_VIDA == "ERROR: #N/A")
 
+data_clean <- data %>% filter(LATITUD != "xx.xxxx" & CURSO_DE_VIDA != "ERROR: #N/A")
 
 
