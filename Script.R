ea4a26968bbe1aefe6301c1521d3eef93459376c
Kevin Quintero <kevinstequintero@gmail.com>
2024-05-26 14:23:08 -0500

    Se creó el script inicial dep proyecto, ademas se ajuntó para que leyera los datos que se encuentrar en el github del proyecto

---
+++ Script.R
@@ -0,0 +1,9 @@
+library(lsr)
+library(moments)
+library(readr)
+
+data <- read.csv("https://raw.githubusercontent.com/kevinQuinteroB/ProyectoEstadistica/main/Delitos_ocurridos_en_el_Municipio_de_Bucaramanga_20240519.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
+
+
+
